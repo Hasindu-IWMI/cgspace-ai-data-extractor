ce4da661cd3b7a234da27572adcf017501691e8c
Hasindu-IWMI <h.ramanayake@cgiar.org>
2025-08-04 22:29:49 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Add Google Chrome repository key and source
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-chrome-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome-archive-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
-# Update package list
-apt-get update
-# Install Chrome, ChromeDriver, and required libraries
-apt-get install -y google-chrome-stable chromedriver libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1 libx11-6 libxext6
